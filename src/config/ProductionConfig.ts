b5f1d3827039adf72183523cd8a77e1a9202626c
yingfai <chowyingfai@gmail.com>
2025-01-07 15:57:20 +0800

    version 9 and setted the domains on website and deployed

--- src/config/ProductionConfig.ts
+++ src/config/ProductionConfig.ts
@@ -1,3 +1,3 @@
 export default {
-    baseUrl:"http://ec2-52-221-246-129.ap-southeast-1.compute.amazonaws.com:8080"
+    baseUrl:"https://api.miumiucatworld.net"
 }
--- src/ui/component/TopNavBar/index.tsx
+++ src/ui/component/TopNavBar/index.tsx
@@ -3,7 +3,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Button, Spinner} from "react-bootstrap";
-import Image from 'react-bootstrap/Image';
 import {Link} from "react-router-dom";
 import {useContext} from "react";
 import {LoginUserContext} from "../../../context/loginUserContext.ts";
@@ -56,7 +55,8 @@ export default function TopNavBar(){
             <Navbar style={{ backgroundColor: '#fbe6e6' }} data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand>
-                        <Image src="src/image/logo.jpg" width={"116px"} roundedCircle/>
+                        {/*<Image src="src/image/logo.jpg" width={"116px"} roundedCircle/>*/}
+                        Pet Pet World
                     </Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
